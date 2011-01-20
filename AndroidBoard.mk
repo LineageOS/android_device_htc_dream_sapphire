a809536a0d2dce38e4070e749768283c0e71fc2b
Wes Garner <wesgarner@gmail.com>
2011-01-20 11:54:45 -0600

    D/S: Gingerbread overhaul. This is a combination of 9 commits.
    D/S: Add more product overrides
    D/S: Add Gingerbread GPS
    D/S: Add Kineto Compat
    D/S: Enable V8 JavaScript Engine
    D/S: Add AAC encoding profiles
    D/S: Update init files for GBread
    D/S: Add a few product packages
    D/S: Complete cleanup of prebuilt stuff
    D/S: Remove AndroidBoard.mk code (is now legacy)

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2007 The Android Open Source Project
+# Copyright (C) 2009 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,101 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#
+# AndroidBoard.mk is a legacy mechanism to deal with a few
+# edge-cases that can't be managed otherwise. No new rules
+# should be added to this file.
+#
+
 LOCAL_PATH := $(call my-dir)
 
-# from device/htc/sapphire
-file := $(TARGET_OUT_KEYLAYOUT)/sapphire-keypad.kl
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/sapphire-keypad.kl | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/init.sapphire.rc
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/init.sapphire.rc | $(ACP)
-	$(transform-prebuilt-to-target)
+# Least specific includes go first, so that they can get
+# overridden further down
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := sapphire-keypad.kcm
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_KEY_CHAR_MAP)
 
-# from device/htc/dream
-file := $(TARGET_OUT_KEYLAYOUT)/trout-keypad.kl
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/trout-keypad.kl | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_OUT_KEYLAYOUT)/trout-keypad-v2.kl
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/trout-keypad-v2.kl | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_OUT_KEYLAYOUT)/trout-keypad-v3.kl
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/trout-keypad-v3.kl | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_OUT_KEYLAYOUT)/trout-keypad-qwertz.kl
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/trout-keypad-qwertz.kl | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_ROOT_OUT)/init.trout.rc
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/init.trout.rc | $(ACP)
-	$(transform-prebuilt-to-target)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := trout-keypad.kcm
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_KEY_CHAR_MAP)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := trout-keypad-v2.kcm
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_KEY_CHAR_MAP)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := trout-keypad-v3.kcm
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_KEY_CHAR_MAP)
-
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := trout-keypad-qwertz.kcm
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_KEY_CHAR_MAP)
-
-# from AndroidBoardCommon.mk
-file := $(TARGET_OUT_KEYLAYOUT)/h2w_headset.kl
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/h2w_headset.kl | $(ACP)
-	$(transform-prebuilt-to-target)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE := vold.fstab
-LOCAL_MODULE_TAGS := optional
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := wlan.ko
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_MODULE_PATH := $(TARGET_OUT)/lib/modules
-LOCAL_SRC_FILES := $(LOCAL_MODULE)
-include $(BUILD_PREBUILT)
-
-# the system properties for each device, loaded by init
-file := $(TARGET_OUT)/build.sapphire.prop
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/build.sapphire.prop | $(ACP)
-	$(transform-prebuilt-to-target)
-
-file := $(TARGET_OUT)/build.trout.prop
-ALL_PREBUILT += $(file)
-$(file) : $(LOCAL_PATH)/build.trout.prop | $(ACP)
-	$(transform-prebuilt-to-target)
-
+# include the non-open-source counterpart to this file
 -include vendor/htc/dream_sapphire/AndroidBoardVendor.mk
-
